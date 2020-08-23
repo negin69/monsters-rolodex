1333533efc4b3f2d8aa1ef058839bb2277bb8e15
negin69 <negin_mtn@yahoo.com>
2020-08-23 12:20:48 +0430

    Updates

---
+++ precache-manifest.2f5854e9e94ebac4dcb097db11c63396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec365de34ff65c01bca67f2e88a00e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d3afe263b64d128337b6",
+    "url": "/monsters-rolodex/static/css/main.9ce9cdc7.chunk.css"
+  },
+  {
+    "revision": "bd6962eacf5833c79eea",
+    "url": "/monsters-rolodex/static/js/2.0988bd25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0988bd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3afe263b64d128337b6",
+    "url": "/monsters-rolodex/static/js/main.f1affbce.chunk.js"
+  },
+  {
+    "revision": "68f3da880de01cc1cf25",
+    "url": "/monsters-rolodex/static/js/runtime-main.50b46f4b.js"
+  }
+]);
